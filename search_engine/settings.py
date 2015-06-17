31d7ebb5c68a5b913565bed2d17a6a91bdcc058a
nazihshoura <shoura.nazeeh@gmail.com>
2015-06-17 15:35:05 +0000

    seted it up for heroku

--- search_engine/settings.py
+++ search_engine/settings.py
@@ -58,16 +58,16 @@ WSGI_APPLICATION = 'search_engine.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'search_engine',
-#         'USER': 'nazihshoura',
-#         'PASSWORD': 'x/x=1:x=0',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'search_engine',
+        'USER': 'nazihshoura',
+        'PASSWORD': 'x/x=1:x=0',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
